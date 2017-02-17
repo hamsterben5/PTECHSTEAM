d4dab3058a500412094fd1187dd96bb706f4968b
ZachSarvey <zasarvey@live.com>
2017-02-16 23:57:01 -0500

    AI and few scripts
    Added music to main menu, added a script and when the AI touches you, it
    now sends you back to the Main Menu

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour {
+
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.tag == "Player")
+			Application.LoadLevel ("Main Menu");
+	}
+}
