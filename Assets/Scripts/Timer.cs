ff69785011bd5024725f6e092d4bd532fdc7de94
hamsterben5 <hamsterben5@yahoo.com>
2017-02-21 13:55:05 -0500

    Added 3rd person and such

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿private var startTime;
+var textTime : String;
+
+
+function Awake()
+{
+
+
+    startTime = Time.time;
+
+
+}
+
+
+function OnGUI()
+{
+
+
+    var guiTime = Time.time - startTime;
+
+
+    var minutes : int = guiTime / 60;
+    var seconds : int = guiTime % 60;
+    var fraction : int = (guiTime * 100) % 100;
+
+
+    text = String.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, fraction);
+    GUI.Label(Rect(400, 25, 100, 30), textTime);
+
+
+}
