cd2fe35530e25721476440412fb91f87975a9e37
Zach Sarvey <zasarvey@live.com>
2017-02-24 13:46:09 -0500

    Coin Collect
    Not finisehd but a base for collecting coins

---
+++ Assets/Scripts/CoinCollect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour
+{
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("Picked Up!" + hit.gameObject.name);
+    }
+}
