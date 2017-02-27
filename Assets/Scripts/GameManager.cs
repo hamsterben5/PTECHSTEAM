577703fdac7cdb62e1027e2915d96d29c143430e
Zach Sarvey <zasarvey@live.com>
2017-02-27 13:34:07 -0500

    Coin System Finished
    Finished coin system + added coin prefab to make for easy drag and drop
    additions of coins.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public GameObject scoreTextObject;
+    int score;
+    Text scoreText;
+
+
+
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        scoreText = scoreTextObject.GetComponent<Text>();
+        scoreText.text = "Coins: " + score.ToString();
+    }
+
+    public void Collect(int passedValue, GameObject passedObject)
+    {
+        passedObject.GetComponent<Renderer>().enabled = false;
+        passedObject.GetComponent<Collider>().enabled = false;
+        Destroy(passedObject, 1f);
+        score = score + passedValue;
+        scoreText.text = "Coins : " + score.ToString();
+    }
+}
