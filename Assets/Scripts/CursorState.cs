5910772b24cedc6d7265ffaa1b518b8cb2b55441
Zach Sarvey <zasarvey@live.com>
2017-02-28 13:34:03 -0500

    Cursor State
    Testing cursor state script to fix bug with cursor disappearing on UI
    start

---
+++ Assets/Scripts/CursorState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorState : MonoBehaviour {
+
+
+    void Awake()
+    {
+        Cursor.visible = true;
+    }
+		
+
+}
