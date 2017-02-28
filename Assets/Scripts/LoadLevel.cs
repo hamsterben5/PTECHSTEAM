c73064b6c3ed35c26b71df6fa312d5010e854cfa
Zach Sarvey <zasarvey@live.com>
2017-02-28 12:54:23 -0500

    Death Scene Update
    Added a main menu button to the death scene

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -6,6 +6,6 @@ public class LoadLevel : MonoBehaviour {
 
 	void OnTriggerEnter (Collider col) {
 		if (col.gameObject.tag == "Player")
-			Application.LoadLevel ("Main Menu");
+			Application.LoadLevel ("MainMenu");
 	}
 }
