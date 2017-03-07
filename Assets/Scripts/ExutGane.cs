be0bf9936f1c3714437cd75bd42849448046fed3
hamsterben5 <hamsterben5@yahoo.com>
2017-03-07 13:25:03 -0500

    Finished level 2 and much more.
    Got level two working and ESC button exits the game.

---
+++ Assets/Scripts/ExutGane.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("Escape"))
+            Application.Quit();
+    }
+}
